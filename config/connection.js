9462a6036e32b5cd209e67c496fb88be51ac784a
Tomcariello <tomcariello@gmail.com>
2017-10-17 19:27:06 -0400

    enabled config files for heroku deploy

---
+++ config/connection.js
@@ -0,0 +1,28 @@
+//Here is where you make the connection to the database and export and used by the O.R.M.
+
+var mysql = require('mysql');
+var connection;
+
+//If in heroku production environment, create connection based on that information
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else { //use local credentials
+  connection = mysql.createConnection({
+  	
+    port: 3306,
+  	host: 'localhost',
+  	user: 'root',
+   	password: '',
+   	database: 'rl_history'
+   });
+}
+
+ connection.connect(function (err) {
+ 	if (err) {
+ 		console.error('error connecting: ' + err.stack);
+ 		return;
+ 	}
+ 	console.log('connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
